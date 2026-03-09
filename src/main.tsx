54e72697e22f67b32a476a1a2c152d597ab7f87b
Brian Gaitán <bagc_gaico2005@hotmail.com>
2026-03-09 15:04:44 -0600

    Tailwindcss and Material UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { StyledEngineProvider } from '@mui/material/styles'
 import './index.css'
 import App from '@/App.tsx'
+import { CssBaseline, GlobalStyles } from '@mui/material'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+      <CssBaseline />
+      <App />
+    </StyledEngineProvider>
   </StrictMode>
 )
